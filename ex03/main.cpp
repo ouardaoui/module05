c85b1b6d3f9af30762f60b50721eda2f0a4a954e
Ayoub Ouardaoui <ayoubouardaoui3@gmail.com>
2023-09-13 07:17:01 +0100

    living life

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -1,19 +1,18 @@
-#include "ShrubberyCreationForm.hpp"
-#include "RobotomyRequestForm.hpp"
-#include "PresidentialPardonForm.hpp"
+#include "Intern.hpp"
 
 int main()
 {
+    Intern i;
     ShrubberyCreationForm form("home");
     RobotomyRequestForm r("target");
-    PresidentialPardonForm k("abass");
+    AForm *k = i.makeForm("Pardon form","abass");
     Bureaucrat b("omar", 1);
-    k.beSigned(b);
+    k->beSigned(b);
     r.beSigned(b);
     form.beSigned(b);
     b.signForm(form);
     form.execute(b);
-    k.execute(b);
+    k->execute(b);
     r.execute(b);
     return 0;   
 }
